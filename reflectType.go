a570660bb43b8dc5d9d919e560b59ce7658fd657
rapolu12 <rrapolu2020@localhost.localdomain>
2023-11-23 16:33:35 -0500

    adding find a type by reflect pkg, correction

--- reflectType.go
+++ reflectType.go
@@ -9,6 +9,6 @@ func main() {
 	var grades int = 42
 	var message string = "hello world"
 
-	fmt.Printf("variable grades = %v is of type %T \n", grades, reflect.TypeOf(grades))
-	fmt.Printf("variable message = %v is of type %T \n ", message, reflect.TypeOf(message))
+	fmt.Printf("variable grades = %v is of type %v \n", grades, reflect.TypeOf(grades))
+	fmt.Printf("variable message = %v is of type %v \n ", message, reflect.TypeOf(message))
 }

6261bd496411342f413379b43f195d936ef5364d
rapolu12 <rrapolu2020@localhost.localdomain>
2023-11-24 06:28:29 -0500

    adding slices

--- datastructures/multiarray.go
+++ datastructures/multiarray.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
+
+	arr := [3][2]int{{1, 2}, {3, 4}, {5, 6}}
 	
+	
+	//element at 0,0
+	fmt.Println(arr[0][0])
+	
+	//element at 0,1
+	fmt.Println(arr[0][1])
+
+	//element at 3,3
+	fmt.Println(arr[2][1])
 }

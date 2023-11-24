50774abaad8a9858f29d3ce0465b89d8e9aec35c
rapolu12 <rrapolu2020@localhost.localdomain>
2023-11-24 06:18:36 -0500

    adding template

--- datastructures/arrays.go
+++ datastructures/arrays.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main(){
 
-	var fruits [2]string = [2]string{"apples", "oranges"}
+	var fruits [5]string = [5]string{"apples", "oranges","grapes","mango","papaya"}
 	fmt.Println(fruits)
 
 	//short hand declaration
@@ -18,6 +18,29 @@ func main(){
 	fmt.Println(len(names))
 
 	//access element at index 0 and 1 , as arrays starts with 0
-	fmt.Println(names[0])
-	fmt.Println(names[1])
+	fmt.Println(fruits[0])
+	fmt.Println(fruits[1])
+
+	//access element 4
+	fmt.Println(fruits[3])
+
+	//update element of names at index 0
+	fmt.Println("===================================")
+	fmt.Println(names)
+	names[0] = "Sham"
+	fmt.Println(names)
+
+	//loop through the array
+
+	fmt.Println("================LOOP===================")
+	for i :=0; i<len(fruits); i++ {
+		fmt.Println(fruits[i])
+	}
+
+	fmt.Println("================RANGE===================")
+
+	for index, elemenet := range fruits {
+		fmt.Println(index, "=>", elemenet)
+	}
+
 }
